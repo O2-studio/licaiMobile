04348aebf1c57ad36cbde36efeea9b3211258d22
frozenluo <frozenluo@gmail.com>
2015-04-03 00:03:06 +0800

    first tab incomplete

---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('fundCtrl', function($scope) {
+    $scope.items = [
+        {
+            name:'创富1期',
+            ing: true,
+            staff: {
+                name: '蒋勇',
+                score: 35.82
+            },
+            score: 21.26,
+            minimal: '100万元'
+        }
+    ];
+})
+;
