0d7051426d07092c3e02bcc1ea867ef2d007ea73
frozenluo <frozenluo@gmail.com>
2015-04-19 23:29:04 +0800

    MOD: 添加咨询预约按钮

--- www/js/app.js
+++ www/js/app.js
@@ -7,96 +7,121 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.filters'])
 
-.config(function($httpProvider){
+.config(function ($httpProvider) {
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-    .state('tab.fund-detail', {
-        url: '/dash/:fundId',
-        views: {
-            'tab-chats': {
-              templateUrl: 'templates/fund-detail.html',
-              controller: 'fundDetailCtrl'
+    .state('tab.dash', {
+            url: '/dash',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/tab-dash.html',
+                    controller: 'DashCtrl'
+                }
             }
-        }
-    })
+        })
+        .state('tab.fund-detail', {
+            url: '/dash/:fundId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/fund-detail.html',
+                    controller: 'fundDetailCtrl'
+                }
+            }
+        })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-  .state('xintuo', {
-    url: '/xintuo',
-    templateUrl: 'templates/xintuo.html',
-      controller: 'trustCtrl'
-  })
-  ;
+    .state('tab.account', {
+            url: '/account',
+            views: {
+                'tab-account': {
+                    templateUrl: 'templates/tab-account.html',
+                    controller: 'AccountCtrl'
+                }
+            }
+        })
+        .state('xintuo', {
+            url: '/xintuo',
+            templateUrl: 'templates/trusts.html',
+            controller: 'trustsCtrl'
+                /*,
+                            views: {
+                                'trust-content': {
+                                    templateUrl: 'templates/trust-selected-list.html',
+                                    controller: 'SelectedTrustsCtrl'
+                                }
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+                            }*/
+        })
+        .state('selected', {
+            url: '/selected',
+            templateUrl: 'templates/trust-selected-list.html',
+            controller: 'SelectedTrustsCtrl'
+//            views: {
+//                'trust-content': {
+//                    templateUrl: 'templates/trust-selected-list.html',
+//                    controller: 'SelectedTrustsCtrl'
+//                }
+//            }
+        })
+
+    .state('detail', {
+        url: '/:id',
+        templateUrl: 'templates/trust-detail.html',
+        controller: 'trustCtrl'
+
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 });
